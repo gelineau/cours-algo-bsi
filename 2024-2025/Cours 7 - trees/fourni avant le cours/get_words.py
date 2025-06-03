46ceb6c6bbcec3a1e84e6a15fee05623affe8114
Francois Gelineau <francois.gelineau@orange.com>
2025-06-03 09:47:16 +0200

    Add cours 7 files before lesson

---
+++ 2024-2025/Cours 7 - trees/fourni avant le cours/get_words.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+# pip install bs4
+# pip install requests
+from bs4 import BeautifulSoup
+import requests
+import re
+
+url = "https://fr.wikipedia.org/wiki/France"
+filename = "words.txt"
+
+
+def extract_text_from_html_page(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Extract the raw text from the HTML
+    text = soup.get_text()
+    return text
+
+
+def get_words(text: str) -> list[str]:
+    # Remove punctuation from the text
+    text = re.sub(r"[^\w\s]", " ", text)
+    # Split the text into words
+    words = text.split()
+    return words
+
+
+raw_text = extract_text_from_html_page(url)
+words = get_words(raw_text)
+
+# change to lowercase, remove words with special characters
+words = {word.lower() for word in words if all(0 <= ord(char) <= 255 for char in word)}
+
+with open(filename, "w") as file:
+    for word in words:
+        file.write(word + "\n")
